77fc15aa3a99e870221832a5885101e3cd514b8a
jsz1 <jacob@mycasebuilder.com>
2015-05-23 10:53:06 -0400

    adding emitter

--- src/noa.js
+++ src/noa.js
@@ -607,6 +607,93 @@
         return el;
     };
 
+    N.Emitter = function(obj) {
+        if (obj){
+            for (var key in Emitter.prototype) {
+                obj[key] = Emitter.prototype[key];
+            }
+            return obj;
+        };
+    };
+
+
+    N.Emitter.prototype.on =
+    N.Emitter.prototype.addEventListener = function(event, fn){
+      this._callbacks = this._callbacks || {};
+      (this._callbacks['$' + event] = this._callbacks['$' + event] || [])
+        .push(fn);
+      return this;
+    };
+
+    N.Emitter.prototype.once = function(event, fn){
+      function on() {
+        this.off(event, on);
+        fn.apply(this, arguments);
+      }
+
+      on.fn = fn;
+      this.on(event, on);
+      return this;
+    };
+
+    N.Emitter.prototype.off =
+    N.Emitter.prototype.removeListener =
+    N.Emitter.prototype.removeAllListeners =
+    N.Emitter.prototype.removeEventListener = function(event, fn){
+      this._callbacks = this._callbacks || {};
+
+      // all
+      if (0 == arguments.length) {
+        this._callbacks = {};
+        return this;
+      }
+
+      // specific event
+      var callbacks = this._callbacks['$' + event];
+      if (!callbacks) return this;
+
+      // remove all handlers
+      if (1 == arguments.length) {
+        delete this._callbacks['$' + event];
+        return this;
+      }
+
+      // remove specific handler
+      var cb;
+      for (var i = 0; i < callbacks.length; i++) {
+        cb = callbacks[i];
+        if (cb === fn || cb.fn === fn) {
+          callbacks.splice(i, 1);
+          break;
+        }
+      }
+      return this;
+    };
+
+    N.Emitter.prototype.emit = function(event){
+      this._callbacks = this._callbacks || {};
+      var args = [].slice.call(arguments, 1)
+        , callbacks = this._callbacks['$' + event];
+
+      if (callbacks) {
+        callbacks = callbacks.slice(0);
+        for (var i = 0, len = callbacks.length; i < len; ++i) {
+          callbacks[i].apply(this, args);
+        }
+      }
+
+      return this;
+    };
+
+    N.Emitter.prototype.listeners = function(event){
+      this._callbacks = this._callbacks || {};
+      return this._callbacks['$' + event] || [];
+    };
+
+    N.Emitter.prototype.hasListeners = function(event){
+      return !! this.listeners(event).length;
+    };
+
     N.Events.prototype.add = function( el, type, handler ) {
         if(N.isJQueryAvailable()){
             $(el).on(type, handler);
@@ -791,6 +878,7 @@
                         query = $(target);
                     } else {
                         query = this.view.el.querySelectorAll( target );
+                        console.log(query)
                     }
 
                     if ( query ) {
